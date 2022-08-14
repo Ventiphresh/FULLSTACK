2b7dbd77c1413f4f0fcba3b8a2d4368c29854826
Anthony <Anthony.L.Ventola@gmail.com>
2022-08-14 19:20:27 -0400

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
-    const dotenv = require('dotenv');
-    dotenv.config();
+    require('dotenv').config();
 }
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -16,7 +16,9 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true })
+    
 const db = mongoose.connection
 
 db.on('error', error => {
