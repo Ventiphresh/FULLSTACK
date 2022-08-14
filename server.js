b42bbe3c8ebac18a7e38b53367407de2fb10d18c
Anthony <Anthony.L.Ventola@gmail.com>
2022-08-14 19:02:52 -0400

    make it better

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const db = mongoose.connection
 db.on('error', error => {
     console.error(error)
 })
-db.once('open', error => {
+db.once('open', () => {
     console.log('Connected to Mongoose')
 })
 
